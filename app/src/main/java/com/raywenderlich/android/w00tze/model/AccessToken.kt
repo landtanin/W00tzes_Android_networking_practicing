563f1e66b3c469a3fa98ee0ed875e8a4fb9a9655
Tanin Rojanapiansatith <sp_land@hotmail.com>
2018-11-16 10:11:52 +0000

    Creating authentication, add filter intent for browser auth, add auth keys in build.gradle, create AuthApi

---
+++ app/src/main/java/com/raywenderlich/android/w00tze/model/AccessToken.kt
@@ -0,0 +1,10 @@
+package com.raywenderlich.android.w00tze.model
+
+import com.google.gson.annotations.SerializedName
+
+class AccessToken(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("token_type") val tokenType: String
+) {
+
+}
---
+++ app/src/main/java/com/raywenderlich/android/w00tze/repository/AuthApi.kt
@@ -0,0 +1,17 @@
+package com.raywenderlich.android.w00tze.repository
+
+import com.raywenderlich.android.w00tze.model.AccessToken
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface AuthApi {
+  @Headers("Accept: application/json")
+  @POST("login/oauth/access_token")
+  @FormUrlEncoded
+  fun getAccessToken(@Field("client_id") clientId: String,
+                     @Field("client_secret") clientSecret: String,
+                     @Field("code") accessCode: String): Call<AccessToken>
+}
